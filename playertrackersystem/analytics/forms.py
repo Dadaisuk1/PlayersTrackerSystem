8bb68628bad06998b005007a353d28b91ca65c6c
MutsarelaCheese <Darwindarryljean.largoza@gmail.com>
2024-12-13 09:01:37 +0800

    delete

--- playertrackersystem/analytics/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Analytics
-
-class AnalyticsForm(forms.ModelForm):
-    class Meta:
-        model = Analytics
-        fields = ['kills', 'death', 'assist', 'win_loss']
--- playertrackersystem/playertrackersystem/hero/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Hero
-
-class HeroForm(forms.ModelForm):
-    class Meta:
-        model = Hero
-        fields = ['hero_id', 'hero_name', 'role', 'match', 'image']
